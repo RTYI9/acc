e1ab6a03ab07e6abb4dc5f37a8ae8234141d8b5b
RTYI9 <69012366+RTYI9@users.noreply.github.com>
2020-08-08 23:08:20 +0300

    Add files via upload

---
+++ Amino.py
@@ -0,0 +1,153 @@
+# -*-coding: utf-8 -*
+import time
+import os
+import random
+
+
+red = "\033[0;31m"
+pur = "\033[0;35m"
+gre = "\033[0;32m"
+yel = "\033[0;33m"
+blu = "\033[0;34m"
+cya = "\033[1;36m"
+whi = "\033[0;37m"
+f='y'
+if f == "y":
+    
+    while f == "y":
+
+
+
+
+        os.system("clear")
+        time.sleep(1)
+        print ("" +red)
+        print ("             _   _   _____   _       _       _____  "+gre)
+        print ("            | | | | | ____| | |     | |     /  _  \ "+cya)
+        print ("            | |_| | | |__   | |     | |     | | | | "+yel)
+        print ("            |  _  | |  __|  | |     | |     | | | | "+pur)
+        print ("            | | | | | |___  | |___  | |___  | |_| | "+blu)
+        print ("            |_| |_| |_____| |_____| |_____| \_____/")  
+        time.sleep(1)
+        os.system("clear")
+        print ("" +red )
+
+        print ("                   _____   _____    _____  "+gre)
+        print ("                  /  _  \ |  _  \  /  _  \ "+cya)
+        print ("                  | |_| | | |_| |  | |_| | "+pur)
+        print ("                  }  _  { |  _  /  \___  | "+yel)
+        print ("                  | |_| | | | \ \   ___| | "+blu)
+        print ("                  \_____/ |_|  \_\ |_____/") 
+        time.sleep (1)
+        os.system("clear")
+        print ("" +red)
+        print("               ___       ___  ___   _   __   _   _____  "+gre)
+        print("              /   |     /   |/   | | | |  \ | | /  _  \ "+cya)
+        print("             / /| |    / /|   /| | | | |   \| | | | | | "+pur)
+        print("            / / | |   / / |__/ | | | | | |\   | | | | | "+blu)
+        print("           / /  | |  / /       | | | | | | \  | | |_| | "+yel)
+        print("          /_/   |_| /_/        |_| |_| |_|  \_| \_____/")
+
+        time.sleep(1)
+
+        os.system("clear")
+
+        print ("" +red )
+        print("        ___   _____   _____   _____   _   _   __   _   _____   _____  "+gre)
+        print("       /   | /  ___| /  ___| /  _  \ | | | | |  \ | | |_   _| /  ___/ "+cya)
+        print("      / /| | | |     | |     | | | | | | | | |   \| |   | |   | |___  "+pur)
+        print("     / / | | | |     | |     | | | | | | | | | |\   |   | |   \___  \ "+yel)
+        print("    / /  | | | |___  | |___  | |_| | | |_| | | | \  |   | |    ___| | "+blu)
+        print("   /_/   |_| \_____| \_____| \_____/ \_____/ |_|  \_|   |_|   /_____/ ")
+        time.sleep(1)
+        os.system("clear")
+
+        time.sleep(1)
+        print ("" +red)
+        print("\t                         __________")
+        print("\t                      .~#########%%;~.")
+        print("\t                     /#&$&#*^#*#&$*÷&$")
+        print("\t                    /#&$&#*#&$*#&$&#*#&")
+        print("\t                   |#######\    /;;;;.,.|")
+        print("\t                   |#########\/%;;;;;.,.|")
+        print("\t          XX       |##/~~\####%;;;/~~\;,|       XX")
+        print("\t        XX..X      |#|  o  \##%;/  o  |.|      X..XX")
+        print("\t      XX.....X     |##\____/##%;\____/.,|     X.....XX")
+        print("\t XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX")
+        print("\tX |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X")
+        print("\tX |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X")
+        print("\tX  \...X     @#%,.@   |# $ & $ ; ; ;,|   @#%,.@     X.../  X")
+        print("\t X# \.X        @#%,.@                  @#%,.@        X./  £")
+        print("\t  $%  X          @#%,.@              @#%,.@          X   &")
+        print("\t, $^ $$            @#%,.@          @#%,.@            X &&")
+        print("\t   `###X             @#%,.@      @#%,.@             $$$$$")
+        print("\t  . ' ><>              @#%.,@  @#%,.@              $$$`")
+        print("\t    . ==                @#%.@#%,.@                ;$% ' .")
+        print("\t      '                    @#%,.@                   ,.")
+        print("\t        ` ,                @#%,.@  @@                `")   
+        print("\t                          @@@  @@@")  
+        time.sleep(1)
+        print ("\n")
+        print ("" +red)
+
+        print ("                           _____   _____    _____  ")
+        print ("                          /  _  \ |  _  \  /  _  \ ")    
+        print ("                          | |_| | | |_| |  | |_| | ")    
+        print ("                          }  _  { |  _  /  \___  | ")    
+        print ("                          | |_| | | | \ \   ___| | ")    
+        print ("                          \_____/ |_|  \_\ |_____/")     
+
+        time.sleep (1)
+        print ("\n")
+        print ("\n")
+        print ("" +pur)
+        x=input ("\n\n\n                              Tybe 8R9 Plz : ")
+        if x == "8R9" :
+
+         
+
+            #[5/8, 2:58 am] ْْ: you@ainbz.com
+            #[5/8, 2:58 am] ْْ: ajha@ainbz.com
+            #[5/8, 2:58 am] ْْ: hjah@ainbz.com
+
+            #[5/8, 10:10 pm] ْْ: iddo@ainbzcom
+
+            #list=["lanixi8801@demail3.com","bahns@ainbz.com","id_frv@ainbz.com","oooooie@ainbz.com","jhsiahgsbw@cartmails.com","dmmktb6893@ascaz.net","7e1a263d84@emaillab.xyz","yukrdt1202@ascaz.net","btaips1131@cartmails.com","yfgiq86249@delowd.com","hhga@ainbz.com","coxd@ainbz.com","ioop@ainbz.com","login@ainbz.com","lxc@ainbz.com","gvqxvi8794@delowd.com","nehw@fazmail.net","duxk@ainbz.com","oaks@ainbz.com","change@ainbz.com","brther@ainbz.com","you@ainbz.com","ajha@ainbz.com","hjah@ainbz.com","iddo@ainbz.com","clixkeher@ainbz.com","aa4898d104@emaillab.xyz","e3d16b5c42@emaillab.xyz","515066c972@emaillab.xyz"]
+
+
+
+
+            list=["xpggrun972@mailvk.net : termux "," wdsxwg5828@icanav.net : termux "," cwsglhl967@mailvk.net : termux "," 1b6f8f0346@emaillab.xyz : termux "," 6ca5be97a9@emaillab.xyz : termux "," 32583dda61@emaillab.xyz : termux :","01e7b9cff2@emaillab.xyz : termux "," 563a6b3810@emaillab.xyz : termux ","lanixi8801@demail3.com : mhmd12.","bahns@ainbz.com : mhmd12.","id_frv@ainbz.com : mhmd12.","oooooie@ainbz.com : mhmd12.","jhsiahgsbw@cartmails.com : mhmd12.","dmmktb6893@ascaz.net : mhmd12.","7e1a263d84@emaillab.xyz : mhmd12.","yukrdt1202@ascaz.net : mhmd12.","btaips1131@cartmails.com : mhmd12.","yfgiq86249@delowd.com : mhmd12.","hhga@ainbz.com : mhmd12.","coxd@ainbz.com : mhmd12.","ioop@ainbz.com : mhmd12.","login@ainbz.com : mhmd12.","lxc@ainbz.com : mhmd12.","gvqxvi8794@delowd.com : mhmd12.","nehw@fazmail.net : mhmd12.","duxk@ainbz.com : mhmd12.","oaks@ainbz.com : mhmd12.","change@ainbz.com : mhmd12.","brther@ainbz.com : mhmd12.","you@ainbz.com mhmd12.","ajha@ainbz.com mhmd12.","hjah@ainbz.com : mhmd12.","iddo@ainbz.com : mhmd12.","clixkeher@ainbz.com : mhmd12.","aa4898d104@emaillab.xyz : mhmd12.","e3d16b5c42@emaillab.xyz : mhmd12.","515066c972@emaillab.xyz","c9022d3403@emaillab.xyz : 8R98R9 "," fda860937c@emaillab.xyz : 8R98R9 "," 73a126e694@emaillab.xyz : 8R98R9 ","a59c91713a@emaillab.xyz :8R98R9 "," aeed4486cc@emaillab.xyz : 8R98R9 "," 992956931b@emaillab.xyz : 8R98R9 "," e8c2b0abe9@emaillab.xyz : 8R98R9 "," de5a8829f7@emaillab.xyz : 8R98R9 "," a7c41f0832@emaillab.xyz : 8R98R9 "," 3d5abe005d@emaillab.xyz : 8R98R9 "," b010dfe034@emaillab.xyz : 8R98R9","d4c0c4f8c2@emaillab.xyz : 8R98R9 "," e333dd113b@emaillab.xyz : 8R98R9 "," ce8d756099@emaillab.xyz : 8R98R9 "," d6d3be33d0@emaillab.xyz : 8R98R9 "," c933d31bc8@emaillab.xyz : 8R98R9 "," 81ead44c23@emaillab.xyz : 8R98R9 "," a7305088fc@emaillab.xyz : 8R98R9 "," d4c0c4f8c2@emaillab.xyz : 8R98R9 "," e78fbe2202@emaillab.xyz : 8R98R9 "," ea093db55a@emaillab.xyz : 8R98R9 "," 0720ae0d9d@emaillab.xyz : 8R98R9 "," de4759be0d@emaillab.xyz : 8R98R9 "," 5d9c67badd@emaillab.xyz : 8R98R9 "," 4b77c943d7@emaillab.xyz : 8R98R9 "," 46c8369c34@emaillab.xyz : 8R98R9 "," 51cf6c0972@emaillab.xyz : 8R98R9 "," d95e467822@emaillab.xyz : 8R98R9 "," 8b50878f5e@emaillab.xyz : 8R98R9 "," 1a6fa485f8@emaillab.xyz : 8R98R9 "," 15470ed1ee@emaillab.xyz :8R98R9 "," 527c58ee07@emaillab.xyz : 8R98R9 "," 9b68f57975@emaillab.xyz : 8R98R9 "," 87121c725e@emaillab.xyz : 8R98R9 "," 34a1d426da@emaillab.xyz : 8R98R9 "," 99931fbb9d@emaillab.xyz : 8R98R9 "," 0f4d6bd491@emaillab.xyz : 8R98R9 "," 7088619128@emaillab.xyz : 8R98R9 "," f389bf2fec@emaillab.xyz : 8R98R9 "]
+
+
+
+
+
+
+
+
+
+
+
+
+
+            w=random.choice(list)
+
+
+            time.sleep(3)
+
+            print ("" + whi)
+            print (" Amino Account :")
+
+            print (w)
+            time.sleep(2)
+            print ("" +cya)
+            print ("\n\n  ====================" +gre)
+            print ("\n\n   My Channel : 8R9AN")
+            print ("\n\n   My insta : asta_199")
+            print ("\n\nMy Amino : http://aminoapps.com/p/8oo9yv ")
+            print ("\n\n   My Group : AL 8RA9NA"+cya)
+            print ("\n\n  ====================")
+            time.sleep(2)
+            f=input ("\nReplay ? y/n  :  ")
+            os.system("clear")
